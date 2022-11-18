1d125346a905933a56138a3f054acae4c9691065
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-18 16:03:26 +0200

    Add PrintDayOfWeek project

--- ObjectOrientedProgrammingChapterTwo/PrintDayOfWeek/Program.cs
+++ ObjectOrientedProgrammingChapterTwo/PrintDayOfWeek/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.DayOfWeek);
+
         }
     }
 }
