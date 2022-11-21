2fe7ca9e2658cd7926857343df8067f5840acfca
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-21 14:06:21 +0200

    Add Tomcat.class

---
+++ ObjectOrientedProgrammingChapterTwo/Animals/Tomcat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        public Tomcat(string name, byte age, string gender) : base(name, age, gender)
+        {
+        }
+        public override string Gender
+        {
+            get => base.Gender;
+            set => base.Gender = "male";
+        }
+    }
+}
