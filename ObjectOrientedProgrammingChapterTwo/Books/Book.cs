479a2d78b5035008a9831b8545302a2498fc68d5
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-21 13:52:33 +0200

    Add refactored Book.class

--- ObjectOrientedProgrammingChapterTwo/Books/Book.cs
+++ ObjectOrientedProgrammingChapterTwo/Books/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +14,11 @@ namespace Books
         private string author;
         private double price;
         private static int minTitleLenght = 3;
-        public Book()
+        public Book(string title, string author, double price)
         {
+            Title = title;
+            Author = author;
+            Price = price;
         }
         public string Title
         {
@@ -25,7 +29,7 @@ namespace Books
 
             set
             {
-                if (title.Length < minTitleLenght)
+                if (value.Length < minTitleLenght)
                 {
                     throw new IOException("Title not valid");
                 }
@@ -42,7 +46,7 @@ namespace Books
 
             set
             {
-                string[] fullName = title.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] fullName = value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 string secondName = fullName[fullName.Length - 1];
                 bool c = Char.IsDigit(secondName, 0);
                 if (c == true)
@@ -54,7 +58,7 @@ namespace Books
 
         }
 
-        public double Price
+        public virtual double Price
         {
             get
             {
@@ -62,15 +66,16 @@ namespace Books
             }
             set
             {
-                if (price <= 0)
+                if (value <= 0)
                 {
                     throw new IOException("Price not valid.");
                 }
+                price = value;
             }
         }
         public override string ToString()
         {
-            return $"{title} - {author} - price: {price}";
+            return $"{title} - {author} - price: {price:0.0}";
         }
     }
 }
