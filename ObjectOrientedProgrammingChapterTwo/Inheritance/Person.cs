ad21c9da2c4259d58c8b081534abee505c5383d4
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-21 14:08:48 +0200

    Add constructor

--- ObjectOrientedProgrammingChapterTwo/Inheritance/Person.cs
+++ ObjectOrientedProgrammingChapterTwo/Inheritance/Person.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,30 +8,38 @@ namespace Inheritance
 {
     public class Person : IEquatable<Person>, IComparable<Person>
     {
-        public string Name {
-            get
-            {
-                return this.Name;
-                    }
-            set {
-                this.Name = value;
-            } }
-        private byte age;
+        public string name;
+        public byte age;
         private Guid Guid { get; set; }
 
         public Person(string name, byte age)
         {
+            this.name = name;
+
             Guid = Guid.NewGuid();
         }
-       
-        public virtual byte Age 
+        public string Name
         {
-            get 
+            get
             {
-                return this.age;
+                return name;
+
             }
-            set {
-                this.age = value;
+
+        }
+        public virtual byte Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new IOException("Age should be positive.");
+                }
+                age = value;
             }
         }
         public override string ToString()
@@ -54,48 +61,6 @@ namespace Inheritance
 
             return other.Age.CompareTo(this.Age);
         }
-
     }
 }
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace Inheritance
-//{
-//    public class Person : IEquatable<Person>, IComparable<Person>
-//    {
-//        public string Name { get; set; }
-//        public virtual byte Age { get; set; }
-//        private Guid Guid { get; set; }
-
-//        public Person(string name, byte age)
-//        {
-//            this.Name = name;
-//            this.Age = age;
-//            Guid = Guid.NewGuid();
-//        }
-//        public override string ToString()
-//        {
-//            return $"Person has name {Name} and it is {Age} years old. Person's ID is {Guid}";
-//        }
-
-//        public bool Equals(Person? other)
-//        {
-//            if (other == null)
-//                return false;
-//            return other is Person person &&
-//                   Name == person.Name &&
-//                   Age == person.Age && Guid == person.Guid;
-//        }
-//        public int CompareTo(Person? other)
-//        {
-//            if (other == null) return 1;
-
-//            return other.Age.CompareTo(this.Age);
-//        }
-//    }
-//}
 
