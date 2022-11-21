76e27363ec57566cf99919b1415a9087d4ba6227
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-21 13:54:42 +0200

    Add GoldenEditionBook.class

---
+++ ObjectOrientedProgrammingChapterTwo/Books/GoldenEditionBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books;
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook(string title, string author, double price) : base(title, author, price)
+    {
+    }
+
+    public override double Price
+    {
+        get => base.Price;
+        set => base.Price = value * 0.3 + value;
+    }
+
+}
+
+
