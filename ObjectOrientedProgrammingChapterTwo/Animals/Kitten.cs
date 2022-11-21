fcd6cc42d2729e20dde7199d606ccf80c85af5e2
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-21 14:04:27 +0200

    Add Kitten.class

---
+++ ObjectOrientedProgrammingChapterTwo/Animals/Kitten.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Kitten : Cat
+    {
+        public Kitten(string name, byte age, string gender) : base(name, age, gender)
+        {
+        }
+        public override string Gender
+        {
+            get => base.Gender;
+            set => base.Gender = "female";
+        }
+
+        public override void ProduceSound()
+        {
+            Console.WriteLine("Miau");
+        }
+    }
+}
