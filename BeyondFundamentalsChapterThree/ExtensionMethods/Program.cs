7d795e3c05b4250de34c3540da4044cbcd41c13c
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-24 10:16:43 +0200

    Add Program.class

--- BeyondFundamentalsChapterThree/ExtensionMethods/Program.cs
+++ BeyondFundamentalsChapterThree/ExtensionMethods/Program.cs
@@ -1,14 +1,27 @@
 ﻿
+using System.Collections;
+
 namespace ExtensionMethods
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            double[] d = { 5, 6, 7 };
+            Console.WriteLine(d.Sum());
+            List<double> n = new List<double>();
+            n.Add(5);
+            n.Add(6);
+            Console.WriteLine(n.Sum());
+            int[] i = { 9, 2, 3, 1, 9, 8, 6, 4 };
+            Console.WriteLine(i.Min());
+            Console.WriteLine(i.Max());
+            Console.WriteLine(d.Average());
 
-            string name = "name";
-            
-           
+
         }
+
+
+
     }
 }
