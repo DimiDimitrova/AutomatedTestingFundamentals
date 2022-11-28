8efa4fa171ee7fc7c9a7c1f418033722496ab94b
DimiDimitrova <dimitrinka.dimitrova@yahoo.com>
2022-11-28 10:53:04 +0200

    Add JavaScript folder

---
+++ JavaScriptCrashCourseChapterNine/VariablesAndDataTypes.js
@@ -0,0 +1,26 @@
+let firstName = prompt("What is your name?");
+console.log(`Your name is ${firstName}!`);
+
+let two = 2;
+let twoHundredAndTwo = 202;
+let twoThAndTwo = 2002;
+console.log(`${two}, ${twoHundredAndTwo}, ${twoThAndTwo}`);
+
+const date = new Date();
+console.log(date);
+
+let number = 32346;
+let squareOfNumber = number * number;
+console.log(squareOfNumber);
+
+let firstDigit = 5;
+undefined
+let secondDigit =10;
+undefined
+let temp= firstDigit;
+undefined
+firstDigit = secondDigit;
+10
+secondDigit=temp;
+5
+console.log(`${firstDigit},${secondDigit}`);
