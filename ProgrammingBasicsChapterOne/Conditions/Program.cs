c5f2c4be660f8abdfc6d98566743fde0da232618
DimiDimitrova <96544050+DimiDimitrova@users.noreply.github.com>
2022-11-30 09:27:55 +0200

    Update Program.cs
    Add a program that shows the sign of the product of three real numbers

--- ProgrammingBasicsChapterOne/Conditions/Program.cs
+++ ProgrammingBasicsChapterOne/Conditions/Program.cs
@@ -26,10 +26,19 @@ namespace Conditions
             }
 
 
-
-
             //2.Write a program that shows the sign of the product of three real numbers without
             //calculating it. Use a sequence of if statements.
+            Console.WriteLine("Enter your first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your first number: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+           
+            if ((a < 0 && b < 0 && c < 0) || (a < 0 && b >= 0 && c >= 0)) Console.WriteLine("-");
+            else if ((a >= 0 && b >= 0 && c >= 0) || (a < 0 && b < 0 && c >= 0)) Console.WriteLine("+");
+            else if ((a < 0 && b >= 0 && c < 0) || (a >= 0 && b < 0 && c < 0)) Console.WriteLine("+");
+            else if ((a >= 0 && b < 0 && c >= 0) || (a >= 0 && b >= 0 && c < 0)) Console.WriteLine("-");
         }
     }
 }
